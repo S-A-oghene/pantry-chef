e2ce526c9930082f17463f80b5627b6ebdc3d31d
unknown <agbamogunjacob@gmail.com>
2026-02-18 16:50:38 +0100

    config: add Edamam API credentials

--- src/js/config.mjs
+++ src/js/config.mjs
@@ -1,7 +1,7 @@
 export const API_KEYS = {
   THE_MEAL_DB: "", // optional (public API)
   EDAMAM: {
-    APP_ID: "", // Replace with your Edamam APP_ID
-    APP_KEY: "", // Replace with your Edamam APP_KEY
+    APP_ID: "e840f874", // Replace with your Edamam APP_ID
+    APP_KEY: "bbad6dbf9019c6594f811ae8939a8c18", // Replace with your Edamam APP_KEY
   },
 };
